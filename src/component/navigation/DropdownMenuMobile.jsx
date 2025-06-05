12b73a925eade74b3d2443f5c1b1721a455aa91e
pak <anhkhuong975@gmail.com>
2025-06-06 01:22:51 +0700

    eIAM: Migrate frontend

--- src/component/navigation/DropdownMenuMobile.jsx
+++ src/component/navigation/DropdownMenuMobile.jsx
@@ -4,6 +4,7 @@ import clsx from 'clsx';
 import LangSelector from '../header/lang-selector/index.jsx';
 import AppContext from '../../common/context/app/app.context.js';
 import useQueryParam from '../../common/hook/useQueryParam.js';
+import ChevronButton from '../../common/ui/ChevronButton.jsx';
 
 /**
  * Dropdown menu for mobile
@@ -18,10 +19,81 @@ const DropdownMenuMobile = () => {
   const [opened, setOpened] = React.useState(false);
 
   // Get context data
-  const {menu} = React.useContext(AppContext);
+  const appContext = React.useContext(AppContext);
+
+  /**
+   * @type {Array<AppMenu & {menuDepth: number}>}
+   */
+  const menu = React.useMemo(() => {
+    const markMenuDepth = (listMenu, currentDepth = 0) => {
+      return listMenu.map((item) => {
+        const updatedItem = {
+          ...item,
+          menuDepth: currentDepth,
+        };
+        if (item.children?.length > 0) {
+          updatedItem.children = markMenuDepth(item.children, currentDepth + 1);
+        }
+        return updatedItem;
+      });
+    };
+
+    return markMenuDepth(appContext.menu);
+  }, [appContext.menu]);
 
   // Get current page
   const currentPage = useQueryParam('c');
+
+  // Menu depth
+  const [currentMenuDepth, setCurrentMenuDepth] = React.useState(0);
+
+  // Rendered list menu state
+  const [renderedListMenu, setRenderedListMenu] = React.useState(menu);
+
+  /**
+   * Handle click expand menu
+   * @type {(menu: AppMenu) => void}
+   */
+  const handleClickExpand = React.useCallback((menu) => {
+    if (menu.children?.length > 0) {
+      setRenderedListMenu(menu.children);
+      setCurrentMenuDepth(menu.children[0].menuDepth);
+    }
+  }, []);
+
+  /**
+   * Handle click back to prev menu
+   *
+   * @type {(function(): void)|*}
+   */
+  const handleClickBack = React.useCallback(() => {
+    if (currentMenuDepth > 0) {
+      const expectMenuDepth = currentMenuDepth - 1;
+
+      // Recursive function to find the menu list at expected depth
+      const findPrevMenuList = (menuList) => {
+        for (const item of menuList) {
+          if (item.menuDepth === expectMenuDepth) {
+            return menuList;
+          }
+
+          if (item.children?.length > 0) {
+            const result = findPrevMenuList(item.children);
+            if (result) {
+              return result;
+            }
+          }
+        }
+        return [];
+      };
+
+      const prevMenuList = findPrevMenuList(menu);
+      if (prevMenuList) {
+        setRenderedListMenu(prevMenuList);
+        setCurrentMenuDepth(expectMenuDepth);
+      }
+    }
+  }, [currentMenuDepth, menu]);
 
   return (
     <>
@@ -42,25 +114,52 @@ const DropdownMenuMobile = () => {
           )}
         >
           {/*region lang selector*/}
-          <div className="p-4 border-b border-b-gray-geyser">
-            <LangSelector />
-          </div>
+          {currentMenuDepth === 0 ? (
+            <div className="p-4 border-b border-b-gray-geyser">
+              <LangSelector />
+            </div>
+          ) : (
+            <div className="px-4 py-4">
+              <div
+                className="cursor-pointer p-1 rounded transition hover:bg-gray-black-squeeze"
+                onClick={handleClickBack}
+              >
+                <ChevronButton
+                  className={clsx(
+                    'transition ',
+                    opened ? 'rotate-45' : 'rotate-90'
+                  )}
+                />
+              </div>
+            </div>
+          )}
+
           {/*endregion lang selector*/}
           {/*region menu*/}
           <nav>
             <ul>
-              {menu.map((menuItem, i) => (
+              {renderedListMenu.map((menuItem, i) => (
                 <li
                   key={i}
                   className={clsx(
-                    'px-4 py-2 border-b transition border-b-gray-geyser border-s-danger-cinnabar',
+                    'flex gap-6 justify-between',
+                    'cursor-pointer px-4 py-2 border-b transition border-b-gray-geyser border-s-danger-cinnabar',
                     'hover:border-s-4',
                     currentPage === menuItem.key ? 'border-s-4' : ''
                   )}
                 >
                   <a href={menuItem.href} className="block !no-underline">
                     {menuItem.label}
-                  </a>{' '}
+                  </a>
+                  {menuItem.children && menuItem.children.length > 0 && (
+                    <ChevronButton
+                      className={clsx(
+                        'px-2 transition',
+                        opened ? '-rotate-45' : 'rotate-90'
+                      )}
+                      onClick={() => handleClickExpand(menuItem)}
+                    />
+                  )}
                 </li>
               ))}
             </ul>
