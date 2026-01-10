9f58986fe665893a4566c3e962dfcf2e6ff731e9
pak <anhkhuong975@gmail.com>
2026-01-10 16:47:45 +0700

    eIAM (u5CMS): Implement Figma design - container

--- src/component/content/Content.jsx
+++ src/component/content/Content.jsx
@@ -17,8 +17,32 @@ import SearchResults from './components/search-results/index.jsx';
 import {ELEMENT_ID} from '../../common/constant/element-id.js';
 import clsx from 'clsx';
 import useQueryParam from '../../common/hook/useQueryParam.js';
+import linkIsCurrentPage from '../../common/helper/linkIsCurrentPage.js';
 
 const isDevMode = import.meta.env.DEV;
+
+/**
+ * Recursively checks if the given menu or any of its child menus is currently active.
+ *
+ * @param {AppMenu} menu
+ * @return {boolean}
+ */
+const hasActivatedMenu = (menu) => {
+  if (linkIsCurrentPage(menu.href)) {
+    return true;
+  } else {
+    // Check deeper levels
+    for (const menuItem of menu.children || []) {
+      if (hasActivatedMenu(menuItem)) {
+        return true;
+      }
+    }
+
+    // If no activated menu found
+    return false;
+  }
+};
+
 /**
  * Content
  *
@@ -30,8 +54,14 @@ const Content = () => {
   const [currentPage] = useQueryParam('c'); // the 'c' letter is the page code of u5CMS
 
   // Get context data
-  const {menu, right, news, content, headerMeta, hasRemovedServerElements} =
-    useContext(AppContext);
+  const {
+    menu: originalMenu,
+    right,
+    news,
+    content,
+    headerMeta,
+    hasRemovedServerElements,
+  } = useContext(AppContext);
 
   // Search contents
   const {isSearchResultPage, searchResults} = useSearchResult(
@@ -43,7 +73,53 @@ const Content = () => {
     offset: (Number(headerMeta.headerHeight) || 10 * 16) + 5 * 16,
   });
 
-  const menus = isDevMode ? mockMenu : menu;
+  /**
+   * Only allow top level items to have children
+   * We don't want to nest to be too deep here
+   *
+   * @type {Array<AppMenu>}
+   */
+  const processedMenus = useMemo(() => {
+    if (isDevMode) {
+      return mockMenu;
+    }
+    const menus = originalMenu;
+
+    /** @see htmltemplate.external-react.html:16 - to see and edit which pages should have sidebar */
+    if (window?.showLeftSidebarPages?.includes(currentPage || '')) {
+      const getCurrentMenu = (listMenu) => {
+        for (const menu of listMenu) {
+          if (menu.key === currentPage) {
+            return menu;
+          }
+          if (menu.children?.length) {
+            const found = getCurrentMenu(menu.children);
+            if (found) {
+              return found;
+            }
+          }
+        }
+        return null;
+      };
+      const currentMenu = getCurrentMenu(menus);
+      if (currentMenu?.children?.length) {
+        return currentMenu.children;
+      } else {
+        return menus.map((menu) => ({
+          ...menu,
+          children: [], // remove children to prevent nested menu for this page
+        }));
+      }
+    }
+
+    for (const menu of menus) {
+      if (hasActivatedMenu(menu)) {
+        return menu.children || [];
+      }
+    }
+    return [];
+  }, [currentPage, originalMenu]);
+
   const rightSidebarContent = isDevMode
     ? mockRightContent
     : [right, news]
@@ -51,20 +127,6 @@ const Content = () => {
         .join('<br>')
         ?.replace(/⇨/g, ArrowRightHTML())
         .replace(/→/g, ArrowRightHTML());
-  const pageContent = isDevMode ? mockContent : content;
-
-  const processedContent = useMemo(() => {
-    if (pageContent) {
-      const arrowRight = ArrowRightHTML({width: 24, height: 24});
-      return stripNavigationMarkers(pageContent)
-        ?.replace(/⇨/g, arrowRight)
-        .replace(/→/g, arrowRight)
-        .replace(/&#8594;/g, arrowRight)
-        .replace(/&amp;#8594;/g, arrowRight);
-    } else {
-      return null;
-    }
-  }, [pageContent]);
 
   /**
    * Show sidebar for specific pages if window.showRightSidebarPages is defined
@@ -83,6 +145,23 @@ const Content = () => {
       return true;
     }
   }, [currentPage, rightSidebarContent]);
+
+  /**
+   * Main content
+   */
+  const pageContent = isDevMode ? mockContent : content;
+  const processedContent = useMemo(() => {
+    if (pageContent) {
+      const arrowRight = ArrowRightHTML({width: 24, height: 24});
+      return stripNavigationMarkers(pageContent)
+        ?.replace(/⇨/g, arrowRight)
+        .replace(/→/g, arrowRight)
+        .replace(/&#8594;/g, arrowRight)
+        .replace(/&amp;#8594;/g, arrowRight);
+    } else {
+      return null;
+    }
+  }, [pageContent]);
 
   useEffect(() => {
     if (processedContent) {
@@ -113,13 +192,14 @@ const Content = () => {
         {/*Element included LeftSidebar and MainContent*/}
         <div
           className={clsx('body-content__container__left', {
-            'justify-center': !menus.length,
+            // Based on design, if there are no left content, center the main content
+            'justify-center': !processedMenus.length,
           })}
         >
           {/*region navigations sidebar*/}
-          {!isSearchResultPage && !!menus.length && (
+          {!isSearchResultPage && !!processedMenus.length && (
             <div className="body-content__left-content">
-              <LeftSidebar menus={menus} />
+              <LeftSidebar menus={processedMenus} />
             </div>
           )}
           {/*endregion navigations sidebar*/}
@@ -132,7 +212,7 @@ const Content = () => {
 
               // Based on design, if there are no right and left content within 'xl' breakpoint, set max width to 225px
               !showRightSidebarPages &&
-                !menu.length &&
+                !processedMenus.length &&
                 'xl:!max-w-225 2xl:!max-w-250'
             )}
           >
--- src/component/left-sidebar/LeftSidebar.jsx
+++ src/component/left-sidebar/LeftSidebar.jsx
@@ -1,85 +1,10 @@
-import React from 'react';
 import LeftMenuItem from './LeftMenuItem.jsx';
-import linkIsCurrentPage from '../../common/helper/linkIsCurrentPage.js';
 import clsx from 'clsx';
-import useQueryParam from '../../common/hook/useQueryParam.js';
-
-const isDevMode = import.meta.env.DEV;
-
-/**
- * Recursively checks if the given menu or any of its child menus is currently active.
- *
- * @param {AppMenu} menu
- * @return {boolean}
- */
-const hasActivatedMenu = (menu) => {
-  if (linkIsCurrentPage(menu.href)) {
-    return true;
-  } else {
-    // Check deeper levels
-    for (const menuItem of menu.children || []) {
-      if (hasActivatedMenu(menuItem)) {
-        return true;
-      }
-    }
-
-    // If no activated menu found
-    return false;
-  }
-};
 
 export default function LeftSidebar({menus}) {
-  // Get current page
-  const [currentPage] = useQueryParam('c'); // the 'c' letter is the page code of u5CMS
-  /**
-   * Only allow top level items to have children
-   * We don't want to nest to be too deep here
-   *
-   * @type {Array<AppMenu>}
-   */
-  const processedMenus = React.useMemo(() => {
-    if (isDevMode) {
-      return menus;
-    }
-
-    /** @see htmltemplate.external-react.html:16 - to see and edit which pages should have sidebar */
-    if (window?.showLeftSidebarPages?.includes(currentPage || '')) {
-      const getCurrentMenu = (listMenu) => {
-        for (const menu of listMenu) {
-          if (menu.key === currentPage) {
-            return menu;
-          }
-          if (menu.children?.length) {
-            const found = getCurrentMenu(menu.children);
-            if (found) {
-              return found;
-            }
-          }
-        }
-        return null;
-      };
-      const currentMenu = getCurrentMenu(menus);
-      if (currentMenu?.children?.length) {
-        return currentMenu.children;
-      } else {
-        return menus.map((menu) => ({
-          ...menu,
-          children: [], // remove children to prevent nested menu for this page
-        }));
-      }
-    }
-
-    for (const menu of menus) {
-      if (hasActivatedMenu(menu)) {
-        return menu.children || [];
-      }
-    }
-    return [];
-  }, [currentPage, menus]);
-
   return (
     <nav className={clsx('left-sidebar')}>
-      {processedMenus.map((menu, index) => (
+      {menus.map((menu, index) => (
         <LeftMenuItem key={index} menu={menu} index={index} />
       ))}
     </nav>
