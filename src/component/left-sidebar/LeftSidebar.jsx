6298dd184b623c1b16053b331229b59c5d4b7d6e
pak <anhkhuong975@gmail.com>
2026-03-01 16:03:23 +0700

    eIAM (u5cms): Side anchors

--- src/component/left-sidebar/LeftSidebar.jsx
+++ src/component/left-sidebar/LeftSidebar.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import LeftMenuItem from './LeftMenuItem.jsx';
 import clsx from 'clsx';
 import {hasChildActive} from '../../common/helper/menu.js';
@@ -6,21 +6,34 @@ import useQueryParam from '../../common/hook/useQueryParam.js';
 
 export default function LeftSidebar({menus}) {
   const [currentPage] = useQueryParam('c');
-
-  const [openIndex, setOpenIndex] = useState(() => {
-    for (let i = 0; i < menus.length; i++) {
-      const menu = menus[i];
-      const hasChildren = menu.children && menu.children.length > 0;
-      if ((!currentPage && i === 0) || (hasChildren && menu.children.some((child) => hasChildActive(child)))) {
-        return i;
-      }
-    }
-    return null;
-  });
+  const [openIndex, setOpenIndex] = useState(null);
 
   const handleToggle = (index) => {
     setOpenIndex((prev) => (prev === index ? null : index));
   };
+
+  /**
+   * Set default open index
+   */
+  useEffect(() => {
+    if (currentPage && menus?.length) {
+      setOpenIndex(() => {
+        for (let i = 0; i < menus.length; i++) {
+          const menu = menus[i];
+
+          const hasChildren = menu.children && menu.children.length > 0;
+          if (
+            (!currentPage && i === 0) ||
+            (hasChildren &&
+              menu.children.some((child) => hasChildActive(child)))
+          ) {
+            return i;
+          }
+        }
+        return null;
+      });
+    }
+  }, [currentPage, menus]);
 
   return (
     <nav className={clsx('left-sidebar')}>
